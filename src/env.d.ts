0e4c8d4932ff9459c4e54970f6afb32bb235cdfe
siat.testing <siat.testing@gmail.com>
2023-12-11 16:59:16 +0000

    Authentication and profile page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+import PocketBase from 'pocketbase';
+
+declare global {
+  declare namespace App {
+    interface Locals {
+      pb: PocketBase;
+    }
+  }
+}
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,4 +1,3 @@
-// src/middleware/index.ts
 import PocketBase from 'pocketbase';
 
 import { defineMiddleware } from 'astro/middleware';
@@ -10,18 +9,22 @@ export const onRequest = defineMiddleware(
     // load the store data from the request cookie string
     locals.pb.authStore.loadFromCookie(request.headers.get('cookie') || '');
 
+    // locals.currentUser = locals.pb.authStore.model;
+
     try {
-      // get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
+      // get an up-to-date auth store state by verifying and refreshing the loaded auth model if any
       locals.pb.authStore.isValid &&
         (await locals.pb.collection('users').authRefresh());
-    } catch (_) {
+      locals.user = locals.pb.authStore.model;
+    } catch (err) {
       // clear the auth store on failed refresh
       locals.pb.authStore.clear();
+      console.error('Error: ', err);
     }
 
     const response = await next();
 
-    // send back the default 'pb_auth' cookie to the client with the latest store state
+    // send back the default 'pb-auth' cookie to the client with the latest store state
     response.headers.append('set-cookie', locals.pb.authStore.exportToCookie());
 
     return response;
